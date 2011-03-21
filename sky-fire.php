44e698ff9b3bbcdea0a91945ace29eeaa542cdce
寡人就是朕 <darkhero@gmail.com>
2011-03-21 13:05:07 +0800

    sky-fire 網站更換網址, 相關網址改為 sfacg.com

--- sky-fire.php
+++ sky-fire.php
@@ -17,7 +17,9 @@ function getSkyfireComic($comic_id,$val,$target_path = null){
 	if(!is_dir($target_path)){
 		mkdir($target_path);
 	}
-	$skyfire_hostnames = array('hotpic.sky-fire.com','coldpic.sky-fire.com','pic2.sky-fire.com');
+	$skyfire_hostnames = array('pic.sfacg.com','pic2.sfacg.com','coldpic.sfacg.com','hotpic.sfacg.com','pic3.sfacg.com');
+	
+	shuffle($skyfire_hostnames);
 
 	foreach($skyfire_hostnames as $skyfire_hostname){
 		$url = "http://{$skyfire_hostname}/Utility/{$comic_id}/{$val}.js";
@@ -37,55 +39,55 @@ function getSkyfireComic($comic_id,$val,$target_path = null){
 	}
 
 	// 有時後會 comic.sky-fire.com 會把圖片放在不同的主機
-	preg_match_all('(http://'.$skyfire_hostname.'/Pic/OnlineComic[0-9]/[[:alnum:]\/._-]*)',$str,$data);
-	echo "本回共".count($data[0])."頁\n";
-	if(count($data[0]) <= 1){
-		preg_match_all('(http://v.sky-fire.com/Temp/[[:alnum:]\/._-]*)',$str,$data);
+	foreach($skyfire_hostnames as $skyfire_hostname){
+		echo $skyfire_hostname;
+		$rex = '(http://'.$skyfire_hostname.'/Pic/OnlineComic[0-9]/[[:alnum:]\/._-]*)';
+		preg_match_all($rex,$str,$data,PREG_PATTERN_ORDER);
+		print_r($data);
+		if(count($data[0]) > 1)
+			break;
 	}
+	if(count($data[0]) <= 1){
+		$rex = '(http://v.sky-fire.com/Temp/[[:alnum:]\/._-]*)';
+		preg_match_all($rex,$str,$data);
+	}
+	echo "本回共".count($data[0])."頁\n";
+
 	$i = 0;
 	foreach($data[0] as $img_src){
 		$i++;
-		$pid = pcntl_fork();
-		if($pid == -1){
-			die('could not fork');
-		}else if($pid){
-			echo 'Fork to '.$pid.' for get '.$img_src."\n";
-		}else{
-			$output_file_name = sprintf('%02d.jpg',$i);
-			if(file_exists($target_path."/".$output_file_name)){
-				echo $target_path."/".$output_file_name." File Exists!\n";
-				exit;
-			}
-			$curl = curl_init();
-
-			$header[] = 'Accept: image/png,*/*;q=0.5';
-			$header[] = 'Accept-Language: zh-tw,en-us;q=0.7,en;q=0.3';
-			$header[] = 'Accept-Encoding: gzip,deflate';
-			$header[] = 'Accept-Charset: Big5,utf-8;q=0.7,*;q=0.7';
-			$cookie = 'cnzz02=3; rtime=0; CommunityServer-UserCookie1=lv=1999-1-1 0:00:00&mra='.$mra.'; ltime=1174363041366; cnzz_eid=52991425-';
-
-			curl_setopt($curl, CURLOPT_URL, $img_src);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-			curl_setopt($curl, CURLOPT_COOKIE,$cookie);
-			curl_setopt($curl, CURLOPT_REFERER,$url.$nowPic.".html");
-			curl_setopt($curl, CURLOPT_HEADER, false);
-			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT,10);
-			//curl_setopt($curl, CURLOPT_PROXY,'proxy.hinet.net');
-			//curl_setopt($curl, CURLOPT_PROXYPORT, 80);
-			$content = curl_exec($curl);
-			curl_close($curl);
-
-			if(strlen($content) > 100){
-				echo "Save: ".$target_path."/".$output_file_name." ".strlen($content)."\n";
-				$fp = fopen($target_path."/".$output_file_name,'w');
-				fputs($fp,$content);
-				fclose($fp);
-				exit;
-			}else{
-				echo "Connect Time Out.Retry \n";
-				exit;
-			}
+		$output_file_name = sprintf('%03d.png',$i);
+		if(file_exists($target_path."/".$output_file_name)){
+			echo $target_path."/".$output_file_name." File Exists!\n";
+			continue;
 		}
-	}
-}
+		$curl = curl_init();
+
+		$header[] = 'Accept: image/png,*/*;q=0.5';
+						  $header[] = 'Accept-Language: zh-tw,en-us;q=0.7,en;q=0.3';
+						  $header[] = 'Accept-Encoding: gzip,deflate';
+						  $header[] = 'Accept-Charset: Big5,utf-8;q=0.7,*;q=0.7';
+						  $cookie = 'cnzz02=3; rtime=0; CommunityServer-UserCookie1=lv=1999-1-1 0:00:00&mra='.$mra.'; ltime=1174363041366; cnzz_eid=52991425-';
+
+						  curl_setopt($curl, CURLOPT_URL, $img_src);
+						  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+						  curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+						  curl_setopt($curl, CURLOPT_COOKIE,$cookie);
+						  curl_setopt($curl, CURLOPT_REFERER,$url.$nowPic.".html");
+						  curl_setopt($curl, CURLOPT_HEADER, false);
+						  curl_setopt($curl, CURLOPT_CONNECTTIMEOUT,10);
+		//curl_setopt($curl, CURLOPT_PROXY,'proxy.hinet.net');
+		//curl_setopt($curl, CURLOPT_PROXYPORT, 80);
+		$content = curl_exec($curl);
+		curl_close($curl);
+
+		if(strlen($content) > 100){
+		echo "Save: ".$target_path."/".$output_file_name." ".strlen($content)."\n";
+		$fp = fopen($target_path."/".$output_file_name,'w');
+		fputs($fp,$content);
+		fclose($fp);
+		}else{
+		echo "Connect Time Out.Retry \n";
+		}
+		}
+		}
